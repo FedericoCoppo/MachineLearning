65b1582aa155d64da93dfddbe03195f47a59f63d
Federico Coppo <54506799+FedericoCoppo@users.noreply.github.com>
2019-12-20 11:24:25 +0100

    collaborative filtering part added.

--- ContentBasedRecommender/contentBasedRecommender.py
+++ ContentBasedRecommender/contentBasedRecommender.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 #print('unziping ...')
 #!unzip -o -j moviedataset.zip 
 
-print("Start script...")
+print("***********************")
+print("CONTENT BASED")
+print("***********************")
 
 # read each file into their Dataframes
 movies_df = pd.read_csv('movies.csv') #Storing the movie information into a pandas dataframe
@@ -49,6 +51,9 @@ moviesWithGenres_df:
 """
 	Content-Based recommendation system: figure out what a user's favourite aspects of an item is, and then recommends items that present those aspects
 	in this case figure out the input's favorite genres from the movies and ratings given.
+	
+	ADVANTAGES: Highly personalized for the user, leanr user preferences
+	DISADVANTAGES: Determining what characteristics of the item the user dislikes or likes is not always obvious, low quality item recommendations might happen
 """
 
 userInput = [
@@ -169,3 +174,108 @@ Recommendation table:
 16055    81132  ...  2010
 26442   122787  ...  1959
 """
+
+"""
+	COLLABORATIVE FILTERING (User-User Filtering): algo used to recommend items to users based on information taken from the user	
+	There are several methods of finding similar users (Correlation Function)
+	
+	- Select a user with the movies the user has watched
+	- Based on his rating to movies, find the top X neighbours
+	- Get the watched movie record of the user for each neighbour.
+	- Calculate a similarity score using some formula
+	- Recommend the items with the highest score
+	
+	Advantages: 
+	- takes consideration of other user's ratings  
+	- do not need to study/extract info from the recommended item
+	- the system adapts to the user's interests which might change over time
+	
+	Disadvantages:
+	- maybe slow approximation function 
+	- privacy issues when trying to learn the user's preferences
+"""
+
+print("***********************")
+print("COLLABORATIVE FILTERING")
+print("***********************")
+
+# Now with the movie ID's in our input, we can now get the subset of users that have watched and reviewed the movies in our input
+userSubset = ratings_df[ratings_df['movieId'].isin(inputMovies['movieId'].tolist())]
+print("user subset table:")
+print(userSubset.head())
+
+# group up the rows by user ID
+userSubsetGroup = userSubset.groupby(['userId']) #. groupby creates several sub dataframes where they all have the same value in the column specified as the parameter
+print("userID=1130:")
+print(userSubsetGroup.get_group(1130))
+
+# sort these groups so the users that share the most movies in common with the input have higher priority (we won't go through every single user)
+userSubsetGroup = sorted(userSubsetGroup,  key=lambda x: len(x[1]), reverse=True) #Sorting it so users with movie most in common with the input will have priority
+
+# Now lets look at the first user
+print(userSubsetGroup[0:3])
+
+# now compare subset of users to our specified user and find the one that is most similar: using Pearson Correlation Coefficient
+# we select a subset of users to iterate through
+userSubsetGroup = userSubsetGroup[0:100]
+
+# calculate the Pearson Correlation between input user and subset group
+pearsonCorrelationDict = {} # Store the Pearson Correlation in a dictionary, where the key is the user Id and the value is the coefficient
+
+for name, group in userSubsetGroup: # for every user group in our subset
+    group = group.sort_values(by='movieId')     #  sort the input and current user group so the values aren't mixed up later on
+    inputMovies = inputMovies.sort_values(by='movieId')
+    nRatings = len(group)     #Get the N for the formula
+    temp_df = inputMovies[inputMovies['movieId'].isin(group['movieId'].tolist())]     # get the review scores for the movies that they both have in common
+    tempRatingList = temp_df['rating'].tolist()                                       # then store them in a temporary buffer variable in a list format to facilitate future calculations
+    tempGroupList = group['rating'].tolist()     									  # put the current user group reviews in a list format
+    
+	#Now let's calculate the pearson correlation between two users, so called, x and y
+    Sxx = sum([i**2 for i in tempRatingList]) - pow(sum(tempRatingList),2)/float(nRatings)
+    Syy = sum([i**2 for i in tempGroupList]) - pow(sum(tempGroupList),2)/float(nRatings)
+    Sxy = sum( i*j for i, j in zip(tempRatingList, tempGroupList)) - sum(tempRatingList)*sum(tempGroupList)/float(nRatings)
+    
+    # If the denominator is different than zero, then divide, else, 0 correlation.
+    if Sxx != 0 and Syy != 0:
+        pearsonCorrelationDict[name] = Sxy/sqrt(Sxx*Syy)
+    else:
+        pearsonCorrelationDict[name] = 0
+
+pearsonCorrelationDict.items()
+pearsonDF = pd.DataFrame.from_dict(pearsonCorrelationDict, orient='index')
+pearsonDF.columns = ['similarityIndex']
+pearsonDF['userId'] = pearsonDF.index
+pearsonDF.index = range(len(pearsonDF))
+
+# Now let's get the top 50 USERS that are most similar to the input
+topUsers=pearsonDF.sort_values(by='similarityIndex', ascending=False)[0:50]
+print(topUsers.head())
+
+# start recommending MOVIES to the input user
+
+# taking the weighted average of the ratings of the movies using the Pearson Correlation as the weight.  
+# 1 get the movies watched by the users in our pearsonDF from the ratings dataframe 
+# 2 store their correlation in a new column called "_similarityIndex"
+# 3 taking the weighted average of the ratings of the movies using the Pearson Correlation as the weight
+topUsersRating=topUsers.merge(ratings_df, left_on='userId', right_on='userId', how='inner')
+
+# multiply the movie rating by its weight (The similarity index)
+topUsersRating['weightedRating'] = topUsersRating['similarityIndex']*topUsersRating['rating']
+topUsersRating.head()
+
+# sum up the new ratings 
+tempTopUsersRating = topUsersRating.groupby('movieId').sum()[['similarityIndex','weightedRating']]
+tempTopUsersRating.columns = ['sum_similarityIndex','sum_weightedRating']
+tempTopUsersRating.head()
+
+# divide it by the sum of the weights
+recommendation_df = pd.DataFrame() #Creates an empty dataframe
+recommendation_df['weighted average recommendation score'] = tempTopUsersRating['sum_weightedRating']/tempTopUsersRating['sum_similarityIndex'] #Now we take the weighted average
+recommendation_df['movieId'] = tempTopUsersRating.index
+recommendation_df.head()
+
+# sort it and see the top 20 movies that the algorithm recommended
+recommendation_df = recommendation_df.sort_values(by='weighted average recommendation score', ascending=False)
+print("RECOMMENDATION LIST:")
+print(recommendation_df.head(10))
+movies_df.loc[movies_df['movieId'].isin(recommendation_df.head(10)['movieId'].tolist())]
